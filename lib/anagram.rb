d003f43bc19f0e9979582a44b02e32abfa9f33a2
Samuel Banya <sbanya@fastmail.com>
2022-08-22 20:27:00 -0400

    Adding latest changes

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :listen
+
+  def initialize(listen)
+    @listen = listen
+  end
+
+  def match(array)
+    # NOTE: There aren't even any hints at all in this lab at all
+    array.select { |word| word.split("").sort == @listen.split("").sort}
+  end
+
+end
