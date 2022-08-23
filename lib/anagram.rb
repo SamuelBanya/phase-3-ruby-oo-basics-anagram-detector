ab8806f98977483159f75d4fbfb68fadfebe3933
Samuel Banya <sbanya@fastmail.com>
2022-08-22 20:23:32 -0400

    commit tree e0669aa00ca785b79bf38779d03f126a92c64d29

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :listen
+
+  def initialize(listen)
+    @listen = listen
+  end
+
+  def match(array)
+    array.select { |word| word.split("").sort == @listen.split("").sort}
+  end
+
+end
